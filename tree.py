cf18f2a13c51a1bb9e455b453b729e8277d0bf22
hindu-muppala <hindumuppala@gmail.com>
2023-09-22 21:31:53 +0530

    initial

---
+++ tree.py
@@ -0,0 +1,22 @@
+class edge():
+    def __init__(self,time):
+        self.time=time
+class node():
+    def __init__(self,value,name):
+        self.value=value
+        self.name=name
+class degree(node):
+    def getName(self):
+        return self.job
+class job(node):
+    def getName(self):
+        return self.name
+class graph():
+    def __init__(self):
+        
+    def traversalByTime():
+
+    def traversalBy
+
+    
+        
