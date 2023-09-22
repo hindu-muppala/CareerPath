95826dbff8bab60487d11c70d328d1ff12a49296
hindu-muppala <hindumuppala@gmail.com>
2023-09-22 21:42:43 +0530

    initial

--- tree.py
+++ tree.py
@@ -9,11 +9,13 @@ class degree(node):
     def getName(self):
         return self.job
 class job(node):
+    def __init__(self,sal):
+        self.sal=sal
     def getName(self):
         return self.name
 class graph():
-    def __init__(self):
-        pass
+    def __init__(self,degree):
+        self.Mainnode=degree(10)
     def traversalByTime(time):
         pass
     def traversalByJob(job):
